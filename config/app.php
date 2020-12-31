31bf10911f6ea7673e1a107dd392069569913a48
wuguangping <wuguangping@qq.com>
2020-12-31 21:18:16 +0800

    增加 IDE助手

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * IDE Helper
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+
     ],
 
     /*
